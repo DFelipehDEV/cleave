ac1e3b056882386ae8064134804482f43185859b
DFelipehDEV <thediogopcm@gmail.com>
2025-04-16 21:28:44 +0100

    added Renderer method to submit vertices and indices

--- runtime/src/rendering/Renderer.cpp
+++ runtime/src/rendering/Renderer.cpp
@@ -23,14 +23,45 @@ void OpenGLRenderer::ClearColor(int r, int g, int b, int a) {
     glClearColor(r / 255.0f, g / 255.0f, b / 255.0f, a / 255.0f);
 }
 
-void OpenGLRenderer::DrawTexture(Texture& texture, float x, float y) {
+void OpenGLRenderer::Draw(Texture* texture, size_t vertexCount, const void* vertexData, const uint32_t* indices, size_t indexCount, Primitive primitive) {
     glActiveTexture(GL_TEXTURE0);
-    texture.Bind();
+    if (texture) texture->Bind();
 
+    unsigned int VAO, VBO, EBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, vertexCount * 4 * sizeof(float), vertexData, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, indexCount * sizeof(uint32_t), indices, GL_STATIC_DRAW);
+
+    // Position attribute
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    // Texture coordinate attribute
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)(2 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    if (primitive == Primitive::Triangle) {
+        glDrawElements(GL_TRIANGLES, static_cast<GLsizei>(indexCount), GL_UNSIGNED_INT, 0);
+    }
+
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
+}
+
+
+void OpenGLRenderer::DrawTexture(Texture& texture, float x, float y) {
     int w = texture.GetWidth();
     int h = texture.GetHeight();
 
-    // Correct vertex data with proper texture coordinates
     float vertices[] = {
         // Positions    // Texture Coords (flipped to match Y Down)
         x,     y + h,   0.0f, 1.0f,  // Bottom-left (now maps to texture bottom-left)
@@ -44,30 +75,5 @@ void OpenGLRenderer::DrawTexture(Texture& texture, float x, float y) {
         0, 2, 3
     };
 
-    unsigned int VAO, VBO, EBO;
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
-    glGenBuffers(1, &EBO);
-
-    glBindVertexArray(VAO);
-
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-    // Position attribute
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-
-    // Texture coordinate attribute
-    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)(2 * sizeof(float)));
-    glEnableVertexAttribArray(1);
-
-    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-
-    glDeleteVertexArrays(1, &VAO);
-    glDeleteBuffers(1, &VBO);
-    glDeleteBuffers(1, &EBO);
+    Draw(&texture, 4, vertices, indices, 6, Primitive::Triangle);
 }
--- runtime/src/rendering/Renderer.hpp
+++ runtime/src/rendering/Renderer.hpp
@@ -8,12 +8,19 @@
 
 class Renderer {
 public:
+    enum Primitive {
+        Triangle
+    };
+
     virtual void Initialize(Window& window) = 0;
     virtual void Terminate() = 0;
 
     virtual void ClearColor(int r, int g, int b, int a) = 0;
 
+    virtual void Draw(Texture* texture, size_t vertexCount, const void* vertexData, const uint32_t* indices, size_t indexCount, Primitive primitive = Primitive::Triangle) = 0;
+
     virtual void DrawTexture(Texture& texture, float x, float y) = 0;
+    //virtual void DrawRect(float x, float y, float width, float height) = 0;
 };
 
 class OpenGLRenderer : Renderer {
@@ -25,7 +32,11 @@ public:
     void Terminate();
 
     void ClearColor(int r, int g, int b, int a);
+
+    void Draw(Texture* texture, size_t vertexCount, const void* vertexData, const uint32_t* indices, size_t indexCount, Primitive primitive = Primitive::Triangle);
+    
     void DrawTexture(Texture& texture, float x, float y);
+    //void DrawRect(float x, float y, float width, float height);
 public:
     glm::mat4 m_projection;
 };
