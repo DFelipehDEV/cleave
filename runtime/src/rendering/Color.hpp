8acc93b49286110da3d9e2e1cfc696f6a7d25676
Zasus <thediogopcm@gmail.com>
2025-09-16 19:13:43 +0100

    add colors

--- runtime/src/rendering/Color.hpp
+++ runtime/src/rendering/Color.hpp
@@ -4,5 +4,18 @@
 namespace Cleave {
 struct Color {
     uint8_t r, g, b, a;
+    Color() : r(0), g(0), b(0), a(255) {}
+    Color(uint8_t red, uint8_t green, uint8_t blue, uint8_t alpha = 255)
+        : r(red), g(green), b(blue), a(alpha) {}
+
+    static Color White()   { return Color(255, 255, 255, 255); }
+    static Color Black()   { return Color(0, 0, 0, 255); }
+    static Color Red()     { return Color(255, 0, 0, 255); }
+    static Color Green()   { return Color(0, 255, 0, 255); }
+    static Color Blue()    { return Color(0, 0, 255, 255); }
+    static Color Yellow()  { return Color(255, 255, 0, 255); }
+    static Color Cyan()    { return Color(0, 255, 255, 255); }
+    static Color Magenta() { return Color(255, 0, 255, 255); }
+    static Color Gray()    { return Color(128, 128, 128, 255); }
 };
 }  // namespace Cleave
